9d414b877d48493ce0437d3ebb0414126e81c17b
Akshay Bist <reddevil.akshay@gmail.com>
2012-09-04 18:55:43 +0530

    Random commit

---
+++ shift_cipher.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Shift cipher
+
+def shift_cipher(plain_text, shift_coeff):
+	cipher_text= ''
+	for c in plain_text:
+		if 32==ord(c):
+			cipher_text+= c
+			continue
+		cipher_text+= chr(65+ (ord(c)-65+shift_coeff%26)%26)
+	return cipher_text
+
+if __name__=='__main__':
+	s= ['THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG', 'TO BE OR NOT TO BE THAT IS THE QUESTION', 'THERE IS NO SPOON', 'DONT PANIC AND CARRY A TOWEL',
+	    'THE CAKE IS A LIE']
+	c= [3, 5, 7, 29, 19]
+	for p, n in zip(s,c):
+		print 'plain_text= "'+p+'"'
+		print 'shift_coeff=',n
+		print 'cipher_text=', shift_cipher(p, n)
